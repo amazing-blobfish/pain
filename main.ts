0d29aaf0ce63689208a8549fb523edf56cbb6aba
amazing-blobfish <125615925+amazing-blobfish@users.noreply.github.com>
2023-10-06 09:39:34 +1000

    help me please

--- main.ts
+++ main.ts
@@ -1,2 +1,12 @@
-maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 12 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 75)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 0) {
+        	
+        }
+    } else {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 75)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    }
+})
