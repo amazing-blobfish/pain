0d8b1f39d015a4ade48e51bee8d65d75da2dedc7
amazing-blobfish <125615925+amazing-blobfish@users.noreply.github.com>
2023-10-06 10:22:55 +1000

    fix like one thing

--- main.ts
+++ main.ts
@@ -1,12 +1,17 @@
+maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 75)
+basic.pause(2200)
+maqueen.motorStop(maqueen.Motors.All)
+maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+basic.pause(280)
+maqueen.motorStop(maqueen.Motors.All)
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 12 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 75)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
-        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 0) {
-        	
-        }
-    } else {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 75)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    while (maqueen.Ultrasonic(PingUnit.Centimeters) < 12 && maqueen.Ultrasonic(PingUnit.Centimeters) > 5) {
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 33)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
+    }
+    while (maqueen.Ultrasonic(PingUnit.Centimeters) < 5) {
+        maqueen.motorStop(maqueen.Motors.All)
     }
 })
